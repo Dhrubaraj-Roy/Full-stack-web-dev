86e046b1133f0329a4019ad29048e8695774e68b
Djrubaraj-Roy <dhrubarajroy1233@gmail.com>
2022-04-19 21:11:01 +0530

    done the list

--- javaScript/jsDOM/script.js
+++ javaScript/jsDOM/script.js
@@ -34,21 +34,29 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function(){
-	if(input.value.length > 0){
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
-		ul.appendChild(li);  
-		input.value = "";
+
+function inputLength(){
+	return input.value.length;
+}
+function addListAfterClick(){
+	if(inputLength() > 0){
+		createListElement();
+		
+	}
+}
+function addListAfterKeypress(event){
+	if(inputLength() > 0 && event.code === "Enter"){
+		createListElement();
 	}
 
-	
+}
 
-}) 
-input.addEventListener("keypress", function(event){
-	if(input.value.length > 0 && event.code === "Enter"){
+function createListElement(){
 		var li = document.createElement("li");
 		li.appendChild(document.createTextNode(input.value));
 		ul.appendChild(li);  
 		input.value = "";
-	}})
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
